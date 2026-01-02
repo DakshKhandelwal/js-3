e42a5a3de84948935460a5ce8185600572f65677
DakshKhandelwal <56481845+DakshKhandelwal@users.noreply.github.com>
2026-01-02 23:10:38 +0530

    Day 17

---
+++ JS/c17/assignments/infiniteScroll/infiniteScroll.js
@@ -0,0 +1,23 @@
+const listItem = document.getElementById("infi-list");
+
+function addItem(count) {
+    for(let i=0; i<count; i++) {
+        const li = document.createElement("li");
+        li.innerText = `Item ${i+1}`;
+        listItem.append(li);
+    }
+}
+
+addItem(10);
+
+function checkScroll() {
+    // console.dir(listItem);
+    // console.log(listItem.scrollHeight);
+    // console.log(listItem.scrollTop);
+    // console.log(listItem.clientHeight);
+    if(listItem.scrollHeight - listItem.scrollTop - listItem.clientHeight < 1) {
+        addItem(2);
+    }
+}
+
+listItem.addEventListener("scroll", checkScroll);
